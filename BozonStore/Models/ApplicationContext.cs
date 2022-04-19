db418a060f7f8e00ab023a6ae820dc06e497ad13
Regber <regber.888@gmail.com>
2022-04-19 11:20:10 +0300

    Add: -Entityframework -Delivery class -ApplicationContext

---
+++ BozonStore/Models/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BozonStore.Models
+{
+    public class ApplicationContext
+    {
+    }
+}
