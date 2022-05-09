b1df2217901a80b33818c9469195fbbb257512a6
Regber <regber.888@gmail.com>
2022-05-09 12:24:58 +0300

    add Shops page and ShopCreate page and action for them in to SellerController

--- BozonStore/Areas/User/Controllers/SellerController.cs
+++ BozonStore/Areas/User/Controllers/SellerController.cs
@@ -3,17 +3,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BozonStore.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace BozonStore.Controllers
 {
     [Area("user")]
+    [Authorize(Roles ="Seller")]
     public class SellerController : Controller
     {
+        ApplicationContext db;
+        public SellerController(ApplicationContext context)
+        {
+            db = context;
+        }
 
         public IActionResult Shops()
         {
+            IEnumerable<Shop> shops = db.Shops.Where(s => s.Seller.Login == User.Identity.Name).ToList();
+            
+            return View(shops);
+        }
+        [HttpGet]
+        public IActionResult CreateShop()
+        {
             return View();
         }
-    }
+        [HttpPost]
+        public IActionResult CreateShop(Shop shop)
+        {
+            db.Sellers.FirstOrDefault(s => s.Login == User.Identity.Name).Shops.Add(shop);
+            db.SaveChanges();
 
+            return RedirectToAction("Shops");
+        }
+    }
 }
