2001f7778b0eeec4d5a63fa470d3712da73671e7
Regber <regber.888@gmail.com>
2022-05-09 19:27:30 +0300

    add CommonController

---
+++ BozonStore/Controllers/CommonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace BozonStore.Controllers
+{
+    public class CommonController : Controller
+    {
+        IWebHostEnvironment env;
+
+        public CommonController(IWebHostEnvironment env)
+        {
+            this.env = env;
+            
+        }
+        public IActionResult GetImage(string path)
+        {
+            var imageDir = env.ContentRootPath.Replace("\\","/");
+            var fullPath = imageDir + path;
+            return PhysicalFile(fullPath, "image/*");
+        }
+    }
+}
