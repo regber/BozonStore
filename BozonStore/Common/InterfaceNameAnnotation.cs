0fde43626bd7e0ef410553f0d49ef836a2171e2f
Regber <regber.888@gmail.com>
2022-05-10 16:57:03 +0300

    Add attibute for annotation interfaces

---
+++ BozonStore/Common/InterfaceNameAnnotation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BozonStore.Common
+{
+    [System.AttributeUsage(System.AttributeTargets.Interface)]
+    public class InterfaceNameAnnotation : System.Attribute
+    {
+        public InterfaceNameAnnotation(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string Name{get;set;}
+    }
+}
