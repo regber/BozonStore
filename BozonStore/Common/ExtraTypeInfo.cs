b9910315f76ba091b807251ef746dc95da7e5b82
Regber <regber.888@gmail.com>
2022-05-11 13:47:14 +0300

    Rename and added methods in ExtraTypeInfo

--- BozonStore/Common/ExtraTypeInfo.cs
+++ BozonStore/Common/ExtraTypeInfo.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace BozonStore.Common
 {
-    public class AnnotationName
+    public static class ExtraTypeInfo
     {
-        private static string GetDisplayName(Type type)
+        private static string GetClassDisplayName(Type type)
         {
             string name = ((System.ComponentModel.DataAnnotations.DisplayAttribute)type
                         .GetCustomAttributes(false)
@@ -17,17 +17,17 @@ namespace BozonStore.Common
         }
         private static string GetInterfaceDisplayName(Type type)
         {
-            string name = ((BozonStore.Common.InterfaceNameAnnotation)(type
+            string name = ((BozonStore.Common.InterfaceDisplayAttribute)(type
                             .GetCustomAttributes(false)
-                            .First(c => c.GetType() == typeof(BozonStore.Common.InterfaceNameAnnotation)))).Name;
+                            .First(c => c.GetType() == typeof(BozonStore.Common.InterfaceDisplayAttribute)))).Name;
 
             return name == null ? string.Empty : name;
         }
-        public static string Get(Type type)
+        public static string GetDisplayName(Type type)
         {
             if (type.IsClass)
             {
-                return GetDisplayName(type);
+                return GetClassDisplayName(type);
             }
             else if (type.IsInterface)
             {
@@ -37,6 +37,15 @@ namespace BozonStore.Common
             {
                 return string.Empty;
             }
+        }
+        public static IEnumerable<Type> GetFirstChildrenOfType(string parentTypeName)
+        {
+            var children = Assembly.GetExecutingAssembly()
+                                   .GetTypes()
+                                   .Where(t => t.GetInterfaces().Any(i => i.Name == parentTypeName) ||
+                                               t.BaseType?.Name == parentTypeName);
+
+            return children == null ? Enumerable.Empty<Type>() : children;
         }
     }
 }
