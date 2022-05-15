4280088a579a2ef48d23dc43a069571b6cd2e9fc
Regber <regber.888@gmail.com>
2022-05-15 18:11:10 +0300

    Rename and replaced ViewComponent _CreateForm->CreateFormVC

--- BozonStore/Areas/Product/ViewComponents/CreateFormVC.cs
+++ BozonStore/Areas/Product/ViewComponents/CreateFormVC.cs
@@ -9,14 +9,14 @@ using System.Linq;
 
 namespace BozonStore.Areas.Product.Views.Product
 {
-    public class _CreateForm : ViewComponent
+    public class CreateFormVC : ViewComponent
     {
 
         public IViewComponentResult Invoke(string productType)
         {
             Type t = Assembly.GetExecutingAssembly().GetType(productType);
             
-            MethodInfo method = typeof(_CreateForm).GetMethod(nameof(_CreateForm.GetView));
+            MethodInfo method = typeof(CreateFormVC).GetMethod(nameof(CreateFormVC.GetView));
             MethodInfo generic = method.MakeGenericMethod(t);
             var view =(ViewViewComponentResult)generic.Invoke(this, null);
 
