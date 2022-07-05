f952383cef0a059041536879a13782e3ae79c1d6
Regber <regber.888@gmail.com>
2022-07-05 17:50:31 +0300

    добавлен тэг хелпер для формирования каталога товаров

---
+++ BozonStore/TagHelpers/CatalogTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using BozonStore.Models.ProductModel;
+using System.Reflection;
+
+
+namespace BozonStore.TagHelpers
+{
+    public class CatalogTagHelper:TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            SetCatalog();
+        }
+
+        private void SetCatalog()
+        {
+
+            var childTypes = Assembly.GetExecutingAssembly()
+                                .GetTypes().Where(t => t.BaseType == typeof(Product));
+                                //.Where(t => t.GetInterfaces().Any(i => i.FullName == parentTypeName) ||
+                                //            t.BaseType?.FullName == parentTypeName);
+        }
+    }
+}
