61e172cfc0cd23c30d585b89c5b92d74212ec5a5
Regber <regber.888@gmail.com>
2022-07-08 17:09:20 +0300

    Добавлен прототип ViewComponent "SearchForm" для поиска товара по группам из каталога, а так же отредактирован "MyStyle.css" для корректного отображения нового мею поиска.

---
+++ BozonStore/ViewComponents/SearchForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System.Reflection;
+
+namespace BozonStore.ViewComponents
+{
+    public class SearchForm:ViewComponent
+    {
+        public IViewComponentResult Invoke(string productType)
+        {
+            Type type = Assembly.GetExecutingAssembly().GetTypes().First(t => t.Name == productType);
+
+            MethodInfo method = typeof(SearchForm).GetMethod(nameof(SearchForm.GetView));
+            MethodInfo generic = method.MakeGenericMethod(type);
+            var view = (ViewViewComponentResult)generic.Invoke(this, null);
+
+            return view;
+        }
+
+        public ViewViewComponentResult GetView<T>() where T : BozonStore.Models.ProductModel.Product
+        {
+            return View<T>(null);
+        }
+    }
+}
