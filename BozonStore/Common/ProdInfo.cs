f907f8365bf13e13e6f1589401cd15d71fdf55d9
Regber <regber.888@gmail.com>
2022-07-12 01:32:32 +0300

    Добавлены методы для получения максимальных и минимальных значений числовых данных для использования данной информации при формировании формы поиска

--- BozonStore/Common/ProdInfo.cs
+++ BozonStore/Common/ProdInfo.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Reflection;
 using Newtonsoft.Json.Linq;
+using BozonStore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace BozonStore.Common
 {
     public static class ProdInfo
     {
+        static ApplicationContext db;
+
+        static ProdInfo()
+        {
+            string connectString = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings")["DefaultConnection"];
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            optionsBuilder.UseSqlServer(connectString);
+
+            db = new ApplicationContext(optionsBuilder.Options);
+        }
+
         public static ArraySegment<TypeInfo> GetProdTypeInfo()
         {
             var interfaces = Assembly.GetEntryAssembly()
@@ -19,17 +34,69 @@ namespace BozonStore.Common
             return interfaces;
         }
 
-        public static string GetProdLimitValues(string productType)
+        public static JObject GetProdPropLimitValues(string productType, IEnumerable<PropertyInfo> properties)
         {
             var type = Assembly.GetEntryAssembly().GetTypes().FirstOrDefault(t=>t.Name== productType);
 
-            var properties = Assembly.GetEntryAssembly()
-                                      .DefinedTypes
-                                      .Where(t => t.IsSubclassOf(type));//t.GetNestedTypes()
+            var childrenTypes = ExtraTypeInfo.GetLastChildrenOfType(type);
 
-            //var b = properties.GetNestedTypes();
-            return "";
+            var products = GetProductsByTypes(childrenTypes.ToArray());
+
+            JObject json = new JObject();
+
+
+
+            foreach(var prop in properties)
+            {
+                if(prop.PropertyType==typeof(int))
+                {
+                    int min = GetProductsMinPropertyValue<int>(prop.Name, products);
+                    int max = GetProductsMaxPropertyValue<int>(prop.Name, products);
+
+                    json.Add(prop.Name + "Min", min);
+                    json.Add(prop.Name + "Max", max);
+                }
+            }
+            
+            return json;
         }
+
+        private static T GetProductsMinPropertyValue<T>(string propertyName, IEnumerable<BozonStore.Models.ProductModel.Product> products) where T: IComparable
+        {
+            //учесть вариант когда в products.count()==0
+            return products.Select(p => (T)p.GetType()
+                                        .GetProperty(propertyName)
+                                        .GetValue(p))
+                                        .Min();
+        }
+        private static T GetProductsMaxPropertyValue<T>(string propertyName, IEnumerable<BozonStore.Models.ProductModel.Product> products) where T : IComparable
+        {
+            //учесть вариант когда в products.count()==0
+            return products.Select(p => (T)p.GetType()
+                                        .GetProperty(propertyName)
+                                        .GetValue(p))
+                                        .Max();
+        }
+
+        public static IEnumerable<PropertyInfo> GetProperyListOfType(Type type)
+        {
+            var properties = type.GetProperties();
+
+            return properties;
+        }
+
+        private static IEnumerable<Models.ProductModel.Product> GetProductsByTypes(params Type[] types)
+        {
+            var products = db.Products.AsNoTracking().ToList();
+
+            products = products.Where(t => types.Any(T => T.Name == t.GetType().Name)).ToList();
+
+            return products;
+        }
+
+        //private static void Get
+
+
     }
 
 
